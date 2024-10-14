bbad641a8212480ad757789e029958ce96810c24
caiyuanrui <caiyuanrui@outlook.com>
2024-10-14 12:47:51 +0800

    Add Cors Policy.

--- src/startup.rs
+++ src/startup.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -9,6 +10,12 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> std::io::Result<Server> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_method()
+                    .allow_any_origin()
+                    .allow_any_header(),
+            )
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subrcribe))
             .app_data(db_pool.clone())
